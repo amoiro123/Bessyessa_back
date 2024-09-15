2dfd1769929fa14dcc4fc756b53808c3217e1f12
hamdi <hamdi.bennosra.dev@gmail.com>
2024-09-15 19:04:18 +0100

    brandDTO ignore brand from productModels list

--- product-service/src/main/java/com/bessy/productservice/dto/BrandDTO.java
+++ product-service/src/main/java/com/bessy/productservice/dto/BrandDTO.java
@@ -1,6 +1,7 @@
 package com.bessy.productservice.dto;
 
 import com.bessy.productservice.enums.ProductType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public class BrandDTO {
     private UUID id;
     private String name;
     private String description;
+    @JsonIgnoreProperties({"brand"})
     private List<ProductModelDTO> productModel;
     private UUID addedBy;
     private ProductType productType;
