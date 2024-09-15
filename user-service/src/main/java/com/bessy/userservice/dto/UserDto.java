c1277c86d5d0006e347c271e6a1ecef91c95f941
hamdi <hamdi.bennosra.dev@gmail.com>
2024-09-15 15:45:35 +0100

    user role

--- user-service/src/main/java/com/bessy/userservice/dto/UserDto.java
+++ user-service/src/main/java/com/bessy/userservice/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.bessy.userservice.dto;
 
+import com.bessy.userservice.enums.Role;
 import com.bessy.userservice.model.UserDetails;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
@@ -10,5 +11,6 @@ public class UserDto {
     private String id;
     private String username;
     private String email;
+    private Role role;
     private UserDetails userDetails;
 }
